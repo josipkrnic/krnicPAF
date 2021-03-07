f6b20302f8de6435d4274b6cdc280a11d8128505
josipkrnic <80029172+josipkrnic@users.noreply.github.com>
2021-03-07 20:21:12 +0100

    Add files via upload

---
+++ 4zadatak.py
@@ -0,0 +1,13 @@
+def pravac(x1,y1,x2,y2):
+    k = (y2-y1)/(x2-x1)
+    l = -k*x + l
+    print("y = {}x + l".format(k,l))
+while True:
+    x1 = float(input("Unesite x koordinatu prve točke: "))
+    y1 = float(input("Unesite y koordinatu prve točke: "))
+    x2 = float(input("Unesite x koordinatu druge točke: "))
+    y2 = float(input("Unesite y koordinatu druge točke: "))
+    if x1 == x2:
+        print("Ponovite unos.")
+    else:
+        print(pravac(x1,y1,x2,y2))
---
+++ 5zadatak.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+def pravac(x1,y1,x2,y2):
+    k = (y2-y1)/(x2-x1)
+    l = -k*x1 + y1
+    print("y = {}x + l".format(k,l))
+while True:
+    x1 = float(input("Unesite x koordinatu prve točke: "))
+    y1 = float(input("Unesite y koordinatu prve točke: "))
+    x2 = float(input("Unesite x koordinatu druge točke: "))
+    y2 = float(input("Unesite y koordinatu druge točke: "))
+    if x1 == x2:
+        print("Ponovite unos.")
+    else:
+        print(pravac(x1,y1,x2,y2))
+        break
+xpoints = np.array([x1,x2])
+ypoints = np.array([y1,y2])
+plt.plot(xpoints,ypoints)
+plt.show()
