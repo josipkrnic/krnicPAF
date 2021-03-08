b7e69e852041a052357a760c5f0916f69a1a2274
josipkrnic <jkrnic@pmfst.hr>
2021-03-08 13:29:41 +0100

    5zadatak - update

--- 5zadatak.py
+++ 5zadatak.py
@@ -3,7 +3,7 @@ import numpy as np
 def pravac(x1,y1,x2,y2):
     k = round((y2-y1)/(x2-x1),3)
     l = -k*x1 + y1
-    print("y = {}x + l".format(k,l))
+    print("y = {}x + {}".format(k,l))
 while True:
     x1 = float(input("Unesite x koordinatu prve točke: "))
     y1 = float(input("Unesite y koordinatu prve točke: "))
@@ -17,4 +17,11 @@ while True:
 xpoints = np.array([x1,x2])
 ypoints = np.array([y1,y2])
 plt.plot(xpoints,ypoints)
-plt.show()
+opcija = input("P ili p za automatsko spremanje, R ili r za unošenje imena: ")
+if opcija == "P" or opcija == "p":
+    plt.show()
+elif opcija == "R" or opcija == "r":
+    ime = input("Unesite ime datoteke: ")
+    plt.savefig(f"{ime}.pdf")
+    plt.show()
+    
