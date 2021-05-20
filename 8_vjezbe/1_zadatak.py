015569dbd3c9e2aad4652d3d3baf37ccee5740ab
josipkrnic <80029172+josipkrnic@users.noreply.github.com>
2021-05-20 09:10:48 +0200

    Delete 8_vjezbe directory

--- 8_vjezbe/1_zadatak.py
+++
@@ -1,8 +0,0 @@
-import Projectile as prj
-import matplotlib.pyplot as plt
-p1 = prj.Projectile()
-p1.init(50, 0, 0, 45)
-print("Uz v0 = {} i kut = {}, domet kosog hica jest {} metara.".format(50, 45, round(p1.range(),3)))
-p1.range()
-p1.plot()
-p1.reset()
